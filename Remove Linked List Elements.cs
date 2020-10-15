7dcd700ecefc1005f9501f26656a202d3a330d4f
Kamrul_Hasan <48267671+Sara-Hasan@users.noreply.github.com>
2020-10-15 17:24:24 +0600

    Add files via upload

---
+++ Remove Linked List Elements.cs
@@ -0,0 +1,88 @@
+https://leetcode.com/problems/remove-linked-list-elements/
+c#:
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val)
+	{
+		while(head!=null)
+        {
+			if (head.val == val)
+			{
+				head = head.next;
+			}
+			else break;
+        }
+		ListNode pre = new ListNode(val + 1,head),ans = head;
+
+		while(head!=null)
+        {
+			if (head.val == val)
+			{
+				pre.next = head.next;
+			}
+			else pre = head;
+			head = head.next;
+        }
+		return ans;
+	}
+}
+
+------------------------------------------------------------------------------------
+c++:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        while(head!=NULL) {
+            if(head->val==val) 
+            {
+                head=head->next;
+            }
+            else break;
+        }
+        ListNode *pre = new ListNode(-1);
+        pre->next = head;
+        ListNode *ans = pre->next;
+
+        while (head != NULL)
+        {
+            if (head->val == val)
+            {
+                if(head->next!=NULL)
+                {
+                    pre->next=head->next;
+                }
+                else{ 
+                    //cout<<"pre"<<endl;
+                    pre->next = NULL;
+                    }
+            }
+            else pre = head;
+            head = head->next;
+        }
+        return ans;
+    }
+};
